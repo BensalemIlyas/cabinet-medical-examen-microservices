1de354b9e1b72ba416276839ef4c00ab78b59fb6
ilyasBensalem <ilyashitchens@gmail.com>
2024-12-21 01:29:49 +0100

    Implementation API Gateway avec swagger. Routes pour le dossier medical.

---
+++ Api-Gateway/src/main/java/com/IlyasBensalemM2DFS/Api_Gateway/controller/DossierMedicalController.java
@@ -0,0 +1,97 @@
+package com.IlyasBensalemM2DFS.Api_Gateway.controller;
+
+import com.IlyasBensalemM2DFS.Api_Gateway.model.DossierMedical;
+import com.IlyasBensalemM2DFS.Api_Gateway.model.DossierMedicalComplet;
+import com.IlyasBensalemM2DFS.Api_Gateway.model.VisiteMedicale;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dossier-medicaux")
+public class DossierMedicalController {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${services.dossier-medical-service.url}")
+    String baseUrl;
+
+
+    @GetMapping("/")
+    public List<DossierMedical> getAllDossierMedical(){
+        String url = baseUrl;
+        return this.restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<DossierMedical>>(){}
+        ).getBody();
+    }
+
+    @GetMapping("/{praticienId}/{patientId}")
+    public DossierMedicalComplet getDossierMedical(@PathVariable String praticienId, @PathVariable String patientId) {
+        String url = baseUrl + "/" + praticienId + "/" + patientId;
+
+        ResponseEntity<DossierMedicalComplet> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                DossierMedicalComplet.class
+        );
+        return response.getBody();
+    }
+
+
+    @GetMapping("/{dossierMedicalId}")
+    public DossierMedical getDossierMedical(@PathVariable String dossierMedicalId){
+        String url = baseUrl + "/" + dossierMedicalId;
+
+        ResponseEntity<DossierMedical> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                DossierMedical.class
+        );
+        return response.getBody();
+    }
+
+
+    @PostMapping("/ajout-dossier")
+    public ResponseEntity<String> ajoutDossierMedical(@RequestBody DossierMedical dossierMedical){
+        String url = baseUrl + "/ajout-dossier";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<DossierMedical> requestEntity = new HttpEntity<>(dossierMedical, headers);
+
+        ResponseEntity<Void> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                requestEntity,
+                Void.class
+        );
+        return ResponseEntity.status(response.getStatusCode()).body("Dossier médical ajouté avec succès");
+    }
+
+    @PostMapping("/ajout-visite/{dossierMedicalId}")
+    public ResponseEntity<String> ajouterVisiteToDossierMedical(@PathVariable String dossierMedicalId, @RequestBody VisiteMedicale visiteMedicale){
+        String url = baseUrl + "/ajout-visite/" + dossierMedicalId;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<VisiteMedicale> requestEntity = new HttpEntity<>(visiteMedicale, headers);
+
+        ResponseEntity<Void> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                requestEntity,
+                Void.class
+        );
+        return ResponseEntity.status(response.getStatusCode()).body("Visite ajoutée avec succès au dossier médical");
+    }
+
+}
---
+++ Api-Gateway/src/main/java/com/IlyasBensalemM2DFS/Api_Gateway/model/DossierMedicalComplet.java
@@ -0,0 +1,50 @@
+package com.IlyasBensalemM2DFS.Api_Gateway.model;
+
+
+import java.util.List;
+
+public class DossierMedicalComplet {
+
+    private String id;
+
+    private Patient patient;
+
+    private Praticien praticien;
+
+    private List<VisiteMedicale> visites;
+
+    public DossierMedicalComplet() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public List<VisiteMedicale> getVisites() {
+        return visites;
+    }
+
+    public void setVisites(List<VisiteMedicale> visites) {
+        this.visites = visites;
+    }
+
+    public Praticien getPraticien() {
+        return praticien;
+    }
+
+    public void setPraticien(Praticien praticien) {
+        this.praticien = praticien;
+    }
+}
